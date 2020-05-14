a008e39078cb11a7e4a0e10814c2ce34b7b911bd
Mirza Rayhan <56281890+mirzarayhan@users.noreply.github.com>
2020-05-14 22:43:17 +0700

    Create Katalog.php

---
+++ application/controllers/Katalog.php
@@ -0,0 +1,32 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Katalog extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        check_not_login();
+        $this->load->model(['Item_m', 'category_m', 'type_m']);
+        $this->load->model('Cetak_m');
+    }
+
+
+    public function index()
+    {
+        $data['row'] = $this->Item_m->get();
+        $this->template->load('template_c', 'client\katalog', $data);
+    }
+
+
+    public function edit($id)
+    {
+        $query = $this->Item_m->get($id);
+        $this->template->load('template_c', 'client\detail');
+    }
+
+}
+
+/* End of file Katalog.php */
